7a68b5798bf4ce377813be78b253a7b30fd43743
RAJVARDHAN SOMRAJ DESHMUKH <RAJSD2610@users.noreply.github.com>
2017-03-31 20:15:01 -0400

    Create simrunCreate.sh
    creates code for 40,60,80,100,130 and W,Wvel,multi

---
+++ simrunCreate.sh
@@ -0,0 +1,71 @@
+# RandomFileCreation
+#41 w
+cd ~/ndnSIM/ns-3/scratch
+sudo chmod 777 40nodesvel.cc
+sudo sed -e 's/NUMBER_OF_NODES 41/NUMBER_OF_NODES 41/g' -e 's/FW_TECH WaitVelStrategy/FW_TECH WaitStrategy/g' <~/ndnSIM/ns-3/40nodesvel.cc> ~/ndnSIM/ns-3/40nodesW.cc
+sudo chmod 777 ~/ndnSIM/ns-3/40nodesW.cc
+#41 mul
+cd ~/ndnSIM/ns-3/scratch
+sudo chmod 777 40nodesvel.cc
+sudo sed -e 's/NUMBER_OF_NODES 41/NUMBER_OF_NODES 41/g' -e 's/FW_TECH WaitVelStrategy/FW_TECH multi/g' <~/ndnSIM/ns-3/40nodesvel.cc> ~/ndnSIM/ns-3/40nodesmulti.cc
+sudo chmod 777 ~/ndnSIM/ns-3/40nodesmulti.cc
+#61 vel
+cd ~/ndnSIM/ns-3/scratch
+sudo chmod 777 40nodesvel.cc
+sudo sed -e 's/NUMBER_OF_NODES 41/NUMBER_OF_NODES 61/g' -e 's/FW_TECH WaitVelStrategy/FW_TECH WaitVelStrategy/g' <~/ndnSIM/ns-3/40nodesvel.cc> ~/ndnSIM/ns-3/60nodesWvel.cc
+sudo chmod 777 ~/ndnSIM/ns-3/60nodesWvel.cc
+#61 W
+cd ~/ndnSIM/ns-3/scratch
+sudo chmod 777 40nodesvel.cc
+sudo sed -e 's/NUMBER_OF_NODES 41/NUMBER_OF_NODES 61/g' -e 's/FW_TECH WaitVelStrategy/FW_TECH WaitStrategy/g' <~/ndnSIM/ns-3/40nodesvel.cc> ~/ndnSIM/ns-3/60nodesW.cc
+sudo chmod 777 ~/ndnSIM/ns-3/60nodesW.cc
+#61 mul
+cd ~/ndnSIM/ns-3/scratch
+sudo chmod 777 40nodesvel.cc
+sudo sed -e 's/NUMBER_OF_NODES 41/NUMBER_OF_NODES 61/g' -e 's/FW_TECH WaitVelStrategy/FW_TECH multi/g' <~/ndnSIM/ns-3/40nodesvel.cc> ~/ndnSIM/ns-3/60nodesmulti.cc
+sudo chmod 777 ~/ndnSIM/ns-3/60nodesmulti.cc
+#81 vel
+cd ~/ndnSIM/ns-3/scratch
+sudo chmod 777 40nodesvel.cc
+sudo sed -e 's/NUMBER_OF_NODES 41/NUMBER_OF_NODES 81/g' -e 's/FW_TECH WaitVelStrategy/FW_TECH WaitVelStrategy/g' <~/ndnSIM/ns-3/40nodesvel.cc> ~/ndnSIM/ns-3/80nodesWvel.cc
+sudo chmod 777 ~/ndnSIM/ns-3/80nodesWvel.cc
+#81 w
+cd ~/ndnSIM/ns-3/scratch
+sudo chmod 777 40nodesvel.cc
+sudo sed -e 's/NUMBER_OF_NODES 41/NUMBER_OF_NODES 81/g' -e 's/FW_TECH WaitVelStrategy/FW_TECH WaitStrategy/g' <~/ndnSIM/ns-3/40nodesvel.cc> ~/ndnSIM/ns-3/80nodesW.cc
+sudo chmod 777 ~/ndnSIM/ns-3/80nodesW.cc
+#81 mul
+cd ~/ndnSIM/ns-3/scratch
+sudo chmod 777 40nodesvel.cc
+sudo sed -e 's/NUMBER_OF_NODES 41/NUMBER_OF_NODES 81/g' -e 's/FW_TECH WaitVelStrategy/FW_TECH multi/g' <~/ndnSIM/ns-3/40nodesvel.cc> ~/ndnSIM/ns-3/80nodesmulti.cc
+sudo chmod 777 ~/ndnSIM/ns-3/80nodesmulti.cc
+#101 vel
+cd ~/ndnSIM/ns-3/scratch
+sudo chmod 777 40nodesvel.cc
+sudo sed -e 's/NUMBER_OF_NODES 41/NUMBER_OF_NODES 101/g' -e 's/FW_TECH WaitVelStrategy/FW_TECH WaitVelStrategy/g' <~/ndnSIM/ns-3/40nodesvel.cc> ~/ndnSIM/ns-3/100nodesWvel.cc
+sudo chmod 777 ~/ndnSIM/ns-3/100nodesWvel.cc
+#101 w
+cd ~/ndnSIM/ns-3/scratch
+sudo chmod 777 40nodesvel.cc
+sudo sed -e 's/NUMBER_OF_NODES 41/NUMBER_OF_NODES 101/g' -e 's/FW_TECH WaitVelStrategy/FW_TECH WaitStrategy/g' <~/ndnSIM/ns-3/40nodesvel.cc> ~/ndnSIM/ns-3/60nodesW.cc
+sudo chmod 777 ~/ndnSIM/ns-3/100nodesW.cc
+#101 mul
+cd ~/ndnSIM/ns-3/scratch
+sudo chmod 777 40nodesvel.cc
+sudo sed -e 's/NUMBER_OF_NODES 41/NUMBER_OF_NODES 101/g' -e 's/FW_TECH WaitVelStrategy/FW_TECH multi/g' <~/ndnSIM/ns-3/40nodesvel.cc> ~/ndnSIM/ns-3/100nodesmulti.cc
+sudo chmod 777 ~/ndnSIM/ns-3/100nodesmulti.cc
+#131 vel
+cd ~/ndnSIM/ns-3/scratch
+sudo chmod 777 40nodesvel.cc
+sudo sed -e 's/NUMBER_OF_NODES 41/NUMBER_OF_NODES 131/g' -e 's/FW_TECH WaitVelStrategy/FW_TECH WaitVelStrategy/g' <~/ndnSIM/ns-3/40nodesvel.cc> ~/ndnSIM/ns-3/130nodesWvel.cc
+sudo chmod 777 ~/ndnSIM/ns-3/130nodesWvel.cc
+#131 w
+cd ~/ndnSIM/ns-3/scratch
+sudo chmod 777 40nodesvel.cc
+sudo sed -e 's/NUMBER_OF_NODES 41/NUMBER_OF_NODES 131/g' -e 's/FW_TECH WaitVelStrategy/FW_TECH WaitStrategy/g' <~/ndnSIM/ns-3/40nodesvel.cc> ~/ndnSIM/ns-3/130nodesW.cc
+sudo chmod 777 ~/ndnSIM/ns-3/130nodesW.cc
+#131 mul
+cd ~/ndnSIM/ns-3/scratch
+sudo chmod 777 40nodesvel.cc
+sudo sed -e 's/NUMBER_OF_NODES 41/NUMBER_OF_NODES 131/g' -e 's/FW_TECH WaitVelStrategy/FW_TECH multi/g' <~/ndnSIM/ns-3/40nodesvel.cc> ~/ndnSIM/ns-3/130nodesmulti.cc
+sudo chmod 777 ~/ndnSIM/ns-3/130nodesmulti.cc
